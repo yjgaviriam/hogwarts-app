c9754d4f28f3ac2fba2d0f12bec94061cbcdf3eb
yjgaviriam <43100577+yjgaviriam@users.noreply.github.com>
2021-02-11 00:37:11 -0500

    Actualiza documentación de filter pipe

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -1,20 +1,36 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+/**
+ * Filtra los elementos de acuerdo al parametro recibido
+ *
+ * @author Jhonier Gaviria M. - Feb. 10-2021
+ * @version 0.1.0
+ */
 @Pipe({
   name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
 
-
+  /**
+   * @see PipeTransform#transform
+   *
+   * @param items Arreglo a filtrar
+   * @param column Parametro de filtrado
+   *
+   * @returns El arreglo ordenado
+   */
   transform(items: any[], search: string): any[] {
 
+    // Si no tiene valor retornamos el arreglo tal cual
     if (search === null || (typeof (search) === 'undefined')) {
       return items;
     }
 
+    // Variables para procesar la respuesta
     const response = [];
     search = search.toLowerCase();
 
+    // Filtramos los items
     for (const item of items) {
       if (item.name.toLowerCase().includes(search) || item.patronus.toLowerCase().includes(search)) {
         response.push(item);
